a121564ceb0b999a1328993681241979e7452a3c
SufyaanMahomed <sufyaan.mahomed77@gmail.com>
2025-04-15 20:09:54 +0200

    Implement signup functionality with error handling and navigation

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 const Signup = () => {
     const [loading, setLoading] = useState("");
@@ -9,6 +10,24 @@ const Signup = () => {
     const [error, setError] = useState("");
 
     const {session, signUpNewUser} = UserAuth();
+    const navigate = useNavigate();
+
+    const handleSignup = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const result = await signUpNewUser(email, password);
+            if (result.success) {
+                navigate('/');
+            } else {
+                setError(result.error.message);
+            }
+        } catch (error) {
+            setError('An error occurred during sign up. Please try again.');
+        }finally {
+            setLoading(false);
+        }
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -22,11 +41,12 @@ const Signup = () => {
 
   return (
     <section>
-        <form className='max-w-md m-auto pt-24'>
+        <form className='max-w-md m-auto pt-24' onSubmit={handleSignup}>
             <h2 className='font-bold pb-2'>Sign Up</h2>
+            {error && <p className='text-red-500 text-center pt-4 pb-4'>{error}</p>}
             <div className='flex flex-col py-2'>
-                <input className='p-3 mt-2 bg-stone-700 rounded-md' type="email" placeholder='Bob@example.com'/>
-                <input className='p-3 mt-2 bg-stone-700 rounded-md' type="password" placeholder='Password'/>
+                <input onChange={(e) => setEmail(e.target.value)} className='p-3 mt-2 bg-stone-700 rounded-md' type="email" placeholder='Bob@example.com'/>
+                <input onChange={(e) => setPassword(e.target.value)} className='p-3 mt-2 bg-stone-700 rounded-md' type="password" placeholder='Password'/>
             </div>
             <button className='mt-6 w-full' type="submit" disabled={loading} >Sign Up</button>
             <p>
