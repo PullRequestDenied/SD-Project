3b3b2b1164608ce46aafb54fb96da8a01c8b4ac5
SufyaanMahomed <sufyaan.mahomed77@gmail.com>
2025-04-15 18:57:56 +0200

    Create UI for Signup Page

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
+    const [loading, setLoading] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        // Simulate a network request
+        setTimeout(() => {
+            setLoading(false)
+            alert('Sign up successful!')
+        }, 2000)
+    }
+
   return (
-    <div>Signup</div>
+    <section>
+        <form className='max-w-md m-auto pt-24'>
+            <h2 className='font-bold pb-2'>Sign Up</h2>
+            <div className='flex flex-col py-2'>
+                <input className='p-3 mt-2 bg-stone-700 rounded-md' type="email" placeholder='Bob@example.com'/>
+                <input className='p-3 mt-2 bg-stone-700 rounded-md' type="password" placeholder='Password'/>
+            </div>
+            <button className='mt-6 w-full' type="submit" disabled={loading} >Sign Up</button>
+            <p>
+                Already have an account? <Link to="/signin">Sign in</Link>
+            </p>
+        </form>
+    </section>
   )
 }
 
