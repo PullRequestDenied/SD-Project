b44a34013d1d4970fab37f6d22c434e544e05fc3
SufyaanMahomed <sufyaan.mahomed77@gmail.com>
2025-04-16 14:31:45 +0200

    Install jwtDecode and Implement RBAC

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { UserAuth } from '../context/AuthContext'
 import { Navigate } from 'react-router-dom';
+import {jwtDecode} from 'jwt-decode'
 
 const PrivateRoute = ({children}) => {
-    const {session,loading} = UserAuth();
+  const {session,loading} = UserAuth();
+
   if (loading) return <div>Loading</div>;
-  return (<>{session ? <>{children}</> : <Navigate to="/signin"/>}</>)
+
+  const token = session?.access_token;
+  const decodedToken = token ? jwtDecode(token) : null;
+  const userRole = decodedToken ? decodedToken.user_role : null;
+
+  return (
+    <>
+      {userRole === 'admin' ? <>{children}</> : <Navigate to="/"/>}
+    </>
+  )
 }
 
 export default PrivateRoute
