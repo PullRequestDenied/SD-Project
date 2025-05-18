b6e5502572ed75855a93a4d3cf1cac1683d28dbb
Muhammad Hoosen <125815271+Momo3945@users.noreply.github.com>
2025-05-18 18:15:42 +0200

    Update server.js
    Test CORS fix

--- api/server.js
+++ api/server.js
@@ -9,7 +9,20 @@ const fileManagerRoutes = require("./routes/fileManager");
 const searchRoutes = require("./routes/searchRoutes");
 const app = express();
 
-app.use(cors({  allowedHeaders: ['Content-Type','Authorization','X-Folder-Id','X-File-Id','X-Tags']}));
+const corsOptions = {
+  origin: 'https://api-sd-project-fea6akbyhygsh0hk.southafricanorth-01.azurewebsites.net',
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Folder-Id',
+    'X-File-Id',
+    'X-Tags'
+  ]
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/filemanager", fileManagerRoutes);
