fa0f29ae9e7041ca4e3851eb2230e0530d45670b
Momo3945 <moohoo03@gmail.com>
2025-05-25 19:55:49 +0200

    put correct link

--- frontend/src/components/FileManagerPage.jsx
+++ frontend/src/components/FileManagerPage.jsx
@@ -25,7 +25,7 @@ export default function FileManagerPage() {
   const { darkMode } = useDarkMode();
 
   const token   = session?.access_token || '';
-  const hostUrl = 'http://localhost:5000';
+  const hostUrl = 'https://api-sd-project-fea6akbyhygsh0hk.southafricanorth-01.azurewebsites.net';
 
   const handleBeforeSend = (args) => {
     args.ajaxSettings.beforeSend = (ajaxArgs) => {
